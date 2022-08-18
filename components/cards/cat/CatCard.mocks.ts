6b282ca012bb5422518b1f14456e5b0b43559eda
Dionisio Vieira <dionisioviei@gmail.com>
2022-08-18 16:37:39 -0300

    feat: add CatCard component

---
+++ components/cards/cat/CatCard.mocks.ts
@@ -0,0 +1,13 @@
+import { ICatCard } from './CatCard';
+
+const base: ICatCard = {
+  tag: 'Felines',
+  title: "What's new in Cats",
+  body: 'Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit..."',
+  author: 'James',
+  time: '2h ago',
+};
+
+export const mockCatCardProps = {
+  base,
+};
